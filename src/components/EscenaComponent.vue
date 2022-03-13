a8726d772a18da28f9729f91d33cb72ee8b4d8c7
Isa <isasimoaynes@gmail.com>
2022-03-13 14:23:16 +0100

    exercici 1

---
+++ src/components/EscenaComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="escena">
+    <p>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</p>
+  </div>
+</template>
+
+<style>
+.escena {
+  background-color: greenyellow;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<script>
+import EscenaComponent from './EscenaComponent.vue'
+
+export default {
+  name: 'HomeComponent',
+  components: {
+    EscenaComponent
+  }
+}
+</script>
+
+<template>
+  <div class="home">
+    <H2>Home</H2>
+    <EscenaComponent />
+  </div>
+</template>
+
+<style>
+.home {
+  background-color: yellow;
+  font-weight: bold;
+  padding: 10px;
+}
+</style>
